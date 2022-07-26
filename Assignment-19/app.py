ab61f3fb7d2b1bef95ab2ba2c962976eea7a4ebd
trivi23 <TRIVIKRAM4567@GMAIL.COM>
2022-07-26 23:42:53 +0530

    Assignment

---
+++ Assignment-19/app.py
@@ -0,0 +1,28 @@
+import cv2
+from deepface import DeepFace
+import time
+
+cam = cv2.VideoCapture(0)
+happy = cv2.imread('happy.png')
+sad = cv2.imread('sad.jpg')
+angry = cv2.imread('angry.png')
+default = cv2.imread('grey.jpg')
+img=default
+while True:
+    status, image = cam.read()
+    if status:
+        try:
+            result = DeepFace.analyze(image, actions=['emotion'])
+            emotion = max(result['emotion'], key=result['emotion'].get)
+            if emotion == "happy":
+                img = happy
+            elif emotion == 'sad':
+                img=sad
+            elif emotion == 'angry':
+                img = angry
+            cv2.imshow("Parent", img)
+        except:
+            img=default
+    if cv2.waitKey(10000) & 0xFF == ord('q'):
+        break
+    cv2.destroyAllWindows()
