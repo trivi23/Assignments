891f2082188976a77ca4aceac2ab5197344d38ba
trivi23 <TRIVIKRAM4567@GMAIL.COM>
2022-07-26 20:08:20 +0530

    Assignment

---
+++ Assignment-16/app.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+import pyttsx3
+import csv
+listener = sr.Recognizer()
+speaker = pyttsx3.init()
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice',voices[1].id)
+
+with sr.Microphone() as source:
+    print("Recording audio")
+    listener.adjust_for_ambient_noise(source, duration=1)
+    recaudio = listener.listen(source)
+    print("recognizing command")
+    try :
+        command = listener.recognize_google(recaudio)
+        command = command.lower()
+        print(f"You Spoke {command}")
+    except:
+        print("Error")
+    
+    if 'alexa get details' in command:
+        command = command.replace('alexa get details', '')
+        speaker.say("Tell your roll number")
+        speaker.runAndWait()
+        roll = listener.recognize_google(listener.listen(source))
+        print(roll)
+        with open('details.csv') as f:
+            head = next(f)
+            reader = csv.reader(f)
+            for r in reader:
+                if r[0] == roll:
+                    print("Name : ", r[1])
+                    print("Percentage : ", r[2])
+                    print("Attendence", r[3])
+                else :
+                    speaker.say("No student Found")
+                    speaker.runAndWait()
